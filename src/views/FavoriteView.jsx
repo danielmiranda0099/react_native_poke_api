16a56f6ab4c53e1e050b0023cc1a8fbb9568dbb8
danielmiranda0099 <danielmesa877@gmail.com>
2023-07-12 10:21:27 -0500

    [ADD] List Favorite Pokemon Cards

--- src/views/FavoriteView.jsx
+++ src/views/FavoriteView.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
+import { Text, View } from "react-native";
+import { getPokemonById, getPokemonFavoriteAPI } from "../api";
+import { PokemonList } from "../components";
+import { useAuth } from "../hooks";
+import { formattingPokemon } from "../utils";
 
 export function FavoriteView() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(JSON.stringify(pokemons, null, 2));
+
+  const { auth } = useAuth();
+
+  const getFavorites = async () => {
+    const response = await getPokemonFavoriteAPI();
+
+    const pokemonsArray = [];
+    for await (const pokemon of response) {
+      const pokemonDetail = await getPokemonById(pokemon);
+      const pokemonFormated = formattingPokemon(pokemonDetail);
+      pokemonsArray.push(pokemonFormated);
+    }
+    
+    setPokemons(pokemonsArray);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        getFavorites();
+      }
+    }, [auth])
+  );
+
+  if (!auth) return <UnregisteredUser />;
+
   return (
     <View>
-      <Text>Favorite</Text>
+      <PokemonList pokemons={pokemons} />
     </View>
-  )
+  );
+}
+
+function UnregisteredUser() {
+  return (
+    <View>
+      <Text>Logeate Para Ver Esto ok</Text>
+    </View>
+  );
 }
